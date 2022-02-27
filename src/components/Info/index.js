137e2cfc2f9e8b8d54329c789594423f0ae4722a
Dummy Redirect for Hackerearth <nagaraj.pandith2002@gmail.com>
2022-02-27 09:02:19 +0530

    checkbox and estimated-budget

--- src/components/Info/index.js
+++ src/components/Info/index.js
@@ -95,7 +95,7 @@ function Information() {
         <div className="container my-12 mx-auto px-4 md:px-12">
         <div className="flex flex-wrap -mx-1 lg:-mx-4">
         <Card 
-                name="Aiport Name"
+                name="Airport Name"
                 maps="/"
                 img="https://picsum.photos/600/400/?random"
                 ratings="5"
@@ -103,7 +103,7 @@ function Information() {
         />
 
         <Card 
-                name="Aiport Name"
+                name="Airport Name"
                 maps="/"
                 img="https://picsum.photos/600/400/?random"
                 ratings="5"
@@ -111,7 +111,7 @@ function Information() {
         />
 
         <Card 
-                name="Aiport Name"
+                name="Airport Name"
                 maps="/"
                 img="https://picsum.photos/600/400/?random"
                 ratings="5"
@@ -143,7 +143,92 @@ function Information() {
         </ul>
         </nav>
 
+        {/* Estimated Budget */}
         <div className="p-2 w-full pt-8 mt-8 border-t bordet-gray-200 text-center"></div>
+        <div className='flex items-center justify-center'>
+        <img alt="Placeholder" className="mb-2 text-center block rounded-full" src="https://cdn-icons-png.flaticon.com/512/3462/3462067.png" width="32" height="32" />
+        <h4 className="ml-2 text-center text-2xl font-normal leading-normal mt-0 mb-2 text-gray-800">Estimated Budget</h4>
+        </div>
+        {/* Cost Table */}
+        <div class="flex flex-col mb-14">
+        <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+        <div class="overflow-hidden">
+                <table class="min-w-full">
+                <thead class="bg-gray-200 border-b">
+                <tr>
+                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                        ID
+                </th>
+                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                        Selected Type
+                </th>
+                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                        Name
+                </th>
+                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                        Expense (₹)
+                </th>
+                </tr>
+                </thead>
+
+                <tbody>
+                <tr class="bg-white border-b">
+                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">1</td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        Hotel
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        Ramesh Restaurant
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                ₹20000
+                </td>
+                </tr>
+
+                <tr class="bg-gray-100 border-b">
+                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">2</td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        Airport
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        Srinagar Airport
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                ₹20009
+                </td>
+                </tr>
+
+                <tr class="bg-white border-b">
+                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">3</td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        Travel 
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        Flight
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                ₹23000
+                </td>
+                </tr>
+
+                <tr class="bg-gray-300 border-b">
+                <td class=""></td>
+                <td class=""></td>
+                <td class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
+                Final Amount Expense
+                </td>
+                <td class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
+                ₹63009
+                </td>
+                </tr>
+                </tbody>
+                </table>
+        </div>
+        </div>
+        </div>
+        </div>
+
     </>
 }
 
